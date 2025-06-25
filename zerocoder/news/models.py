4120b60ca17d4ab7051a0d3a5c82b3f45ee6f53f
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-25 19:22:18 +0300

    create migrations

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class News_post(models.Model):
-    title = models.CharField('Название новости')
+    title = models.CharField('Название новости', max_length=50)
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
